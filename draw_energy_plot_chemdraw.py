9d95b2e3db48845042f4944051c88a4508e1d4d2
snljty <snljty@gmail.com>
2023-12-21 13:59:32 +0800

    make code more elegent

--- draw_energy_plot_chemdraw.py
+++ draw_energy_plot_chemdraw.py
@@ -9,22 +9,17 @@ From Excel to ChemDraw.
 import pandas as pd
 
 itemid: int = 0
-factor: float = 10.0
-iname = 'input.xlsx'
-oname = 'output.cdxml'
-levelLength = 20
-connectionLength = 30
+factor: float = 10.
+levelLength: float = 20.
+connectionLength: float = 30.
 
-data = pd.read_excel(iname)
-
-def PrintBegin() -> None:
+def PrintBegin(oname: str) -> None:
     r'''
 print the initial part, including xml version, CDXML begin, 
 colortable, fonttable, page begin, ...
 '''
-    global oname
-    f = open(oname, 'w')
-    print('''<?xml version="1.0" encoding="UTF-8" ?>
+    with open(oname, 'wt') as f:
+        print('''<?xml version="1.0" encoding="UTF-8" ?>
 <CDXML
  CreationProgram="ChemDraw"
  Name="output.cdxml"
@@ -43,95 +38,99 @@ colortable, fonttable, page begin, ...
         <font id="0" charset="iso-8859-1" name="Arial"/>
     </fonttable>
     <page>''', file = f)
-    f.close()
     return
 
-def PrintEnd() -> None:
+def PrintEnd(oname: str) -> None:
     r'''
 print the final part, including CDXML end and page end.
 '''
-    global oname
-    f = open(oname, 'a')
-    print('''    </page>
+    with open(oname, 'at') as f:
+        print('''    </page>
 </CDXML>
 
 
 ''', file = f)
-    f.close()
     return
 
-def PrintLine(posx: float, posy: float) -> None:
+def PrintLine(oname: str, posx: float, posy: float) -> None:
     r'''
 print a bar line.
 '''
-    global oname, itemid, levelLength
+    global itemid, levelLength
     itemid += 1
-    f = open(oname, 'a')
-    print('''        <arrow
-         id="%d"
+    with open(oname, 'at') as f:
+        print(f'''        <arrow
+         id="{itemid:d}"
          FillType="None"
          ArrowheadType="Solid"
-         Head3D="%.1lf %.1lf 0"
-         Tail3D="%.1lf %.1lf 0"
-        />''' % (itemid, posx, posy, posx + levelLength, posy), file = f)
-    f.close()
+         Head3D="{posx:.1f} {posy:.1f} 0"
+         Tail3D="{posx + levelLength:.1f} {posy:.1f} 0"
+        />''', file = f)
     return
 
-def PrintConnect(posx1: float, posy1: float, posx2: float, posy2: float) -> None:
+def PrintConnect(oname: str, posx1: float, posy1: float, posx2: float, posy2: float) -> None:
     r'''
 print a bar line.
 '''
-    global oname, itemid
+    global itemid
     itemid += 1
-    f = open(oname, 'a')
-    print('''        <arrow
-         id="%d"
+    with open(oname, 'at') as f:
+        print(f'''        <arrow
+         id="{itemid:d}"
          LineType="Dashed"
          FillType="None"
          ArrowheadType="Solid"
-         Head3D="%.1lf %.1lf 0"
-         Tail3D="%.1lf %.1lf 0"
-        />''' % (itemid, posx1, posy1, posx2, posy2), file = f)
-    f.close()
+         Head3D="{posx1:.1f} {posy1:.1f} 0"
+         Tail3D="{posx2:.1f} {posy2:.1f} 0"
+        />''', file = f)
     return
 
-def PrintText(posx: float, posy: float, content: str) -> None:
+def PrintText(oname: str, posx: float, posy: float, content: str) -> None:
     r'''
 print text.
 '''
-    global oname, itemid
+    global itemid
     itemid += 1
-    f = open(oname, 'a')
-    print('''        <t
-         id="%d"
-         p="%.1lf %.1lf"
+    with open(oname, 'at') as f:
+        print(f'''        <t
+         id="{itemid:d}"
+         p="{posx:.1f} {posy:.1f}"
          LineHeight="auto"
         >
-            <s font="0" color="0" size="8">%s</s>
-        </t>''' % (itemid, posx, posy, content), file = f)
-    f.close()
+            <s font="0" color="0" size="8">{content:s}</s>
+        </t>''', file = f)
     return
 
-if __name__ == '__main__':
+def GenerateFile(iname: str='input.xlsx', oname: str='output.cdxml') -> None:
+    # global data
+    if not iname:
+        iname = 'input.xlsx'
+    if not oname:
+        oname = 'output.cdxml'
+    if not iname.endswith('.xlsx'):
+        iname += '.xlsx'
+    if not oname.endswith('.cdxml'):
+        oname += '.cdxml'
     initPosx = 20
     initPosy = 400
-    PrintBegin()
+    data = pd.read_excel(iname)
+    PrintBegin(oname)
     for i in data.index:
-        l = list(data.loc[i])
+        ene = list(data.loc[i])
         newPosx = initPosx
         newPosy = initPosy
         status = False # if any level of this line has been drawn, status is True, otherwise False
-        for levelIndex in range(len(l)):
-            if not pd.isna(l[levelIndex]):
+        for levelIndex in range(len(ene)):
+            if not pd.isna(ene[levelIndex]):
                 # this level should be drawn
-                newPosy = initPosy - factor * l[levelIndex]
+                newPosy = initPosy - factor * ene[levelIndex]
                 if status:
                     # draw a connection line here
                     newPosx += connectionLength
-                    PrintConnect(oldPosx, oldPosy, newPosx, newPosy)
+                    PrintConnect(oname, oldPosx, oldPosy, newPosx, newPosy)
                 # draw a level line here, as well as the text
-                PrintLine(newPosx, newPosy)
-                PrintText(newPosx, newPosy + 8, '%.1lf' % l[levelIndex])
+                PrintLine(oname, newPosx, newPosy)
+                PrintText(oname, newPosx, newPosy + 8, f'{ene[levelIndex]:.2f}')
                 newPosx += levelLength
                 # at least one level has already been drawn, set status to True, so
                 # before any new level in this line is drawn, a connection has to be drawn.
@@ -143,7 +142,23 @@ if __name__ == '__main__':
                 newPosx += (levelLength + connectionLength)
     newPosx = initPosx
     for struname in list(data.columns):
-        PrintText(newPosx, 600, struname)
+        if isinstance(struname, (float, int)):
+            label = f'{float(struname):.2f}'
+        else:
+            label = str(struname)
+        PrintText(oname, newPosx, 600, str(label))
         newPosx += 50
-    PrintEnd()
+    PrintEnd(oname)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) - 1 >= 1:
+        iname = sys.argv[1]
+    else:
+        iname = ""
+    if len(sys.argv) - 1 >= 2:
+        oname = sys.argv[2]
+    else:
+        oname = ""
+    GenerateFile(iname, oname)
 
